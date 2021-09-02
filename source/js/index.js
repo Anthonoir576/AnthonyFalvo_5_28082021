a7b487633fe76c6c47c4b521a857f3de157fa52e
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-02 07:41:40 +0200

    PRIX OK tout est bon, rajout de liens a la place dun cursor pointer, pour diriger vers la page produit, puis on attaque la deuxieme etape, les produits en individuel

--- source/js/index.js
+++ source/js/index.js
@@ -3,7 +3,6 @@
 
 /* declarations de ma variable en globale */
 let cameras;
-const accueil = document.querySelector('.accueil-info');
 const showShop = document.getElementById('myBoutique');
 const buttonShop = document.getElementById('buttonShop');
 
@@ -28,14 +27,15 @@ const showCameras = async() => {
             .map(camera => (
 
                 `
-                
-                <div class="camera-item">
-                    <img class="camera-img" src="${camera.imageUrl}" alt="article en vente" />
-                    <div class="camera-info">
-                        <h2 class="camera-name">${camera.name}</h2>
-                        <p class="camera-price">${camera.price} €</p>
+                <a href="./produits.html" aria-label="Votre selection produit">
+                    <div class="camera-item">
+                        <img class="camera-img" src="${camera.imageUrl}" alt="article en vente" />
+                        <div class="camera-info">
+                            <h2 class="camera-name">${camera.name}</h2>
+                            <p class="camera-price">${pricesSpace(camera.price)} €</p>
+                        </div>
                     </div>
-                </div>
+                </a>
 
                 `
 
@@ -44,6 +44,12 @@ const showCameras = async() => {
 
 };
 
+/* dans un premier temps la fonction utilise un separateur de millier et remplace par un point tous les trois chiffre on parle de tostring et replace. Ensuite on emglobe cette methode dans un parsefloat, suivi dun tous fixed avec pour argument 2 . parseFloat transforme une chaine de caractère en nombre, et to fixed nous donne 2 chiffres après la virgule grace a largument 2. Si javais mis 5, j'en aurait donc .... 5 */
+function pricesSpace(prix) {
+
+    return parseFloat(prix.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')).toFixed(2);
+
+};
 
 /* Ma fonction permet au click d'afficher la boutique via le bouton voir nos produits et créer un bouton fermer pour la masqué */
 
@@ -70,7 +76,7 @@ buttonShop.addEventListener("click", () => {
         );
 
     };
-    
+
 });
 
 
