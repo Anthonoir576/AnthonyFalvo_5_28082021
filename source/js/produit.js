bf0e33b9b8c809b9487e976de49089a883857050
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-10-23 11:04:23 +0200

    refactorisation du code partie produit.js  => En cours

--- source/js/produit.js
+++ source/js/produit.js
@@ -1,31 +1,53 @@
 
 /************* AFFICHAGE INDIVIDUEL DU PRODUIT SELECTIONNER PAR LUTILISATEUR ***************/
 
-/* FONCTION PRINCIPALE */
-/* Fonction recursive declare deux constantes fessant appel une entre elle, cette fonction s'appelle elle même après réponse du serveur grace au await */
-const recuperationProduitSeul = async function() {
+/* ########################################################### */
+/* ---------------- VARIABLE / CONSTANTE --------------------- */
+/* ########################################################### */
+// L'endroit ou l'on injecte le html 
+const mySelection = document.querySelector('.main-produit');
+
+// Message visuel de confirmation
+let confirm;
+
+// Choix option / Quantité de l'utilisateur
+let valueInputLenses;
+let valueInputQuantity;
+
+// recuperation du local storage
+let mesProduitsEnregistrer = JSON.parse(localStorage.getItem("mon panier"));
+
+// Objet utilisateur qui contiendra le choix produit
+let selectionUtilisateur;
+/* ########################################################### */
+/* ----------------------------------------------------------- */ 
+
+
+
+/* ----------------------- FONCTIONS  ------------------------ */
+
+/* ########################################################### */
+/* ---------  RECUPERATION ID PRODUIT INDIVIDUEL  ------------ */
+/* ########################################################### */
+const recuperationProduitSeul = async() => {
  
     const produitId = getProduitId();
     const produit = await getProduit(produitId);
 
+    produitSeul(produit);
+    optionProduit(produit);
     majProduit(produit);
 };
-recuperationProduitSeul();
 
-/* BALISE MAIN en HTML OU SERRA AFFICHER LE PRODUIT */
-const mySelection = document.querySelector('.main-produit');
-
-/* URL PAR ID */
-/* Recupere dans l'url existante une information placé en argument, en l'occurence on cherche a récupéré l'iD du produit, elle est souvent représenté de cette façon '?id=' dans l'url. cette fonction est donc stocker dans une variable et sert d'argument au moment de l'appel de l'url, ce qui permet de nous retourner une url + l'id */
-function getProduitId() {
+// Recuperation ID produit via URL injecter dans la balise <a href="./produit.html?id=${camera._id}"
+const getProduitId = () => {
 
     return new URL(location.href).searchParams.get("id");
 
 };
 
-/* FETCH */
-/* On récupere l'url comme sur l'ensemble des produits, a l'inverse, on y incrémente à la fin l'argument, cette argument fait appel a la fonction getproduitid() récupérant lid. Du coup cette fonction nous return l'url du produit selectionner via une promesse et retourne la reponse en json */
-function getProduit(produitId) {
+// Fetch dynamique en injectant l'id afin d'avoir le produit selectionner
+const getProduit = (produitId) => {
 
     return fetch(`http://localhost:3000/api/cameras/${produitId}`)
     .then(function(response) {
@@ -41,15 +63,24 @@ function getProduit(produitId) {
 
 };
 
+recuperationProduitSeul();
+/* ########################################################### */
+/* ----------------------------------------------------------- */
+
+
 /* CONVERSION CENTIME / EUROS */
-function pricesSpace(prix) {
+const pricesSpace = (prix) => {
 
     return parseFloat(prix / 100);
 
 };
 
-/* 01: Création de la fiche produit individuel */
-let produitSeul = (produit) => {
+
+/* ########################################################### */
+/* ---------- FONCTIONS INCLUES DANS MAJPRODUIT() ------------ */
+/* ########################################################### */
+// 01. AFFICHAGE Produit de facon visuel
+const produitSeul = (produit) => {
 
     mySelection.innerHTML = (
 
@@ -94,9 +125,8 @@ let produitSeul = (produit) => {
 
 };
 
-
-/* 02: Ajoute des lentilles pour chaque produit en option dynamiquement */
-let optionProduit = (produit) => {
+// 02: Ajout des options produit dynamiquement
+const optionProduit = (produit) => {
 
     for(let i = 0; i < produit.lenses.length; i++) {
 
@@ -116,45 +146,131 @@ let optionProduit = (produit) => {
 
 };
 
+// MESSAGE VISUEL + Condition si singulier = 1 / Sinon pluriel
+const confirmationFonction = (produit) => {
 
-/* LA STRUCTURE COMPLETE */
-/* Creation produit - ajoute option - ecoute du clique et agit (voir detail) - Ajout au localStorage */
-function majProduit(produit) {
+    confirm.style.background ="#32CD32";
 
-    /**
-     ******* 01: Création de la fiche produit individuel *******
-     *  => Injection dans le html partie main, une section avec les differentes informations via l'URL
-     * 
-     ******* 02: Ajoute des lentilles pour chaque produit en option dynamiquement *******
-     *  => Boucle FOR qui parcours toute les lentilles du tableau lenses pour chaque produit individuellement via l'id et les ajoute dans le select chaque option dynamiquement . Même si on en rajoute dans le tableau par le back-end.
-     * 
-     ******* 03: Listener qui ecoute l'ajoute du produit au clique et verifie certaine information *******
-     *  => Tel que la quantité qui est envoyé au panier, l'option choisit, et pose une condition SI, l'option et l'id est le même n'ajoute que la quantité afin d'éviter les doublons. Affiche le panier est plein une fois celui ci arrivé a dix articles différents et empêche l'ajoute de tiers, et affiche un message de confirmation soit au singulier, soit pluriel avec le nom produit et l'option. Les produits sont stocker dans le local storage ou un ajout de quantité uniquement dans le local, grace a un systeme de mise a jour manuel de ce dernier.
-     *  => la partie 03 est commenté en totalité Ainsi qu'une explication sur la logique de la condition d'ajout produit au localStorage
-    */
+    if(valueInputQuantity == 1) {
+
+        confirm.textContent = `Merci pour votre confiance. Le ${produit.name} ${valueInputLenses} est ajouté au panier`; 
+
+    } else {
+
+        confirm.textContent = `Merci pour votre confiance. Les ${produit.name} ${valueInputLenses} sont ajoutés au panier`;
+        
+    };
+
+    confirm.insertAdjacentHTML('beforeend', `<i class="far fa-check-square"></i>`);
+
+};
+
+// Fonction push dans le local storage l'objet selectionUtilisateur
+const localSto = (selection, enregistrer) => {
+
+    enregistrer.push(selection);
+    localStorage.setItem("mon panier", JSON.stringify(enregistrer));
+
+};
+
+// Verification du produit selectionner dans le local storage         
+const verificationDesProduits = (produit) => {
+            
+    // si aucun elements n'est present dans le local
+    if (mesProduitsEnregistrer == null && valueInputQuantity >= 0) {
+
+        mesProduitsEnregistrer = [];
+        confirmationFonction(produit);
+        localSto(selectionUtilisateur, mesProduitsEnregistrer);
+        
+    // verification valeur negative input quantité
+    } else if (valueInputQuantity <= 0) {
+
+        confirm.style.background ="red";
+
+        confirm.textContent = `Le nombre ne peut être negatif ...`;
+        confirm.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
+
+        return;
+
+    // si un tableau et present et valeur positive
+    } else if (mesProduitsEnregistrer != null && valueInputQuantity > 0) {
+
+        for(let i = 0; i < mesProduitsEnregistrer.length; i++) {
+
+            // si un produit ID et une meme OPTION sont déjà present alors => MAJ quantité
+            if (mesProduitsEnregistrer[i].id === selectionUtilisateur.id && mesProduitsEnregistrer[i].choice === selectionUtilisateur.choice) {
+            
+                let totalQuantité = parseInt(mesProduitsEnregistrer[i].quantity, 10) + parseInt(selectionUtilisateur.quantity ,10);
+                
+                let updateStorage = mesProduitsEnregistrer;
     
-
-    produitSeul(produit);
-
-
-    optionProduit(produit);
+                updateStorage[i].quantity = totalQuantité;
     
+                mesProduitsEnregistrer = updateStorage;
+                
+                confirmationFonction(produit);
+                localStorage.setItem('mon panier', JSON.stringify(mesProduitsEnregistrer));
+            
+            // tant que l'on a pas fini de parcourir le tableau
+            } else if ( (i + 1) < mesProduitsEnregistrer.length) {
+
+                // Tant que le nombre et inférieur a 10 & supérieur a 0 => on relance l'itération
+                if (mesProduitsEnregistrer.length < 10 && mesProduitsEnregistrer.length > 0) {
+
+                    continue;
+
+                // 10 ARTICLE DIFFERENT MAXIMUM
+                } else if ( mesProduitsEnregistrer.length == 10) {
+
+                    confirm.style.background ="red";
+            
+                    confirm.textContent = `VOTRE PANIER EST PLEIN`;
+                    confirm.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
+        
+                    return;
+
+                };
+            
+            // si le tableau et parcourus en integralité, et que aucun produit nest identique sur lid ou le choix on add
+            } else if ((i + 1) == mesProduitsEnregistrer.length && mesProduitsEnregistrer[i].id != selectionUtilisateur.id || mesProduitsEnregistrer[i].choice != selectionUtilisateur.choice) {
+
+                confirmationFonction(produit);
+                localSto(selectionUtilisateur, mesProduitsEnregistrer);
+
+            };
+
+            return;
+
+        };
+
+    };
+
+};
 
 
-    /* 03: Listener qui ecoute l'ajoute du produit au clique et verifie certaine information  */
+/* ########################################################### */
+/* -----------------   MISE A JOUR PANIER   ------------------ */
+/* ########################################################### */
+
+// Ecoute devenement au clique mettant a jour le localStorage + Message visuel
+const majProduit = (produit) => {
+
     document.getElementById('panier').addEventListener('click', (e) => {
 
         // stop le comportement par defaut du bouton
         e.preventDefault();
 
-        // Element au stocker le message de confirmation ET panier plein SI il l'es
-        let confirm = document.getElementById('confirmationAjoutPanier');
-        // valeur quantité / option
-        let valueInputQuantity = document.getElementById('inputQuantity').value;
-        let valueInputLenses = document.getElementById('choiseLenses').value;
 
-        // Object contenant le choix de l'utilisateur
-        let selectionUtilisateur = {
+        // Element au stocker le message de confirmation ET panier plein SI il l'es
+        confirm = document.getElementById('confirmationAjoutPanier');
+        // valeur quantité / option
+        valueInputQuantity = document.getElementById('inputQuantity').value;
+        valueInputLenses = document.getElementById('choiseLenses').value;
+
+
+        // Objet contenant le choix de l'utilisateur
+        selectionUtilisateur = {
 
             id : produit._id,
             picture : produit.imageUrl,
@@ -165,122 +281,18 @@ function majProduit(produit) {
 
         };
         
-        // recuperation du local storage
-        let mesProduitsEnregistrer = JSON.parse(localStorage.getItem("mon panier"));
 
-        /* Fonction push dans le local storage l'objet selectionUtilisateur */  
-        let localSto = (selection, enregistrer) => {
-
-            enregistrer.push(selection);
-            localStorage.setItem("mon panier", JSON.stringify(enregistrer));
-
-        };
-   
-        // REND VISIBLE le message de confirmation 
+        // Comfirmation visuel d'ajout au panier 
         confirm.style.display = "block"; 
         
-        // MESSAGE VISIBLE + Condition si un article est selectionner appliqué le singulier / Sinon pluriel
-        let confirmationFonction = () => {
 
-            confirm.style.background ="#32CD32";
-
-            if(valueInputQuantity == 1) {
-    
-                confirm.textContent = `Merci pour votre confiance. Le ${produit.name} ${valueInputLenses} est ajouté au panier`; 
-    
-            } else {
-    
-                confirm.textContent = `Merci pour votre confiance. Les ${produit.name} ${valueInputLenses} sont ajoutés au panier`;
-                
-            };
-    
-            confirm.insertAdjacentHTML('beforeend', `<i class="far fa-check-square"></i>`);
-
-        };
-
-        // VERIFICATION DOUBLON - via le même ID et la même option : equivaut au même article add que la quantité
-
-        /**
-         * IF la key mon panier n'existe pas dans le local storage ALORS crée un tableau + fonction de confirmation + injecte le choix utlisateur dans le local storage
-         * 
-         * ELSE IF la key mon panier existe donc, elle est différente de null, du coup je parcours le tableau du local, et j'y repose une condition. Si le choix et lid que lutilisateur a selectionné sont identique a un article dans le local, alors je recupere le local dans un tableau, je met a jour la quantité, et le renvoi. la logique permet de parcourir tous le tableau intégralement, pour vérifier si lelement n'est pas existant. A la fin du tableau si cette element nest pas existant, alors il ajoute le produit au local storage
-         * 
-         * MAJ ajout contrôle nombre NEGATIF. 
-        */
-        let verificationDesProduits = () => {
-            
-            // si aucun elements n'est present dans le local
-            if (mesProduitsEnregistrer == null && valueInputQuantity >= 0) {
-
-                mesProduitsEnregistrer = [];
-                confirmationFonction();
-                localSto(selectionUtilisateur, mesProduitsEnregistrer);
-                
-            // verification valeur negative input quantité
-            } else if (valueInputQuantity <= 0) {
-
-                confirm.style.background ="red";
-        
-                confirm.textContent = `Le nombre ne peut être negatif ...`;
-                confirm.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
-    
-                return;
-            // si un tableau et present 
-            } else if (mesProduitsEnregistrer != null && valueInputQuantity > 0) {
-
-                for(let i = 0; i < mesProduitsEnregistrer.length; i++) {
-
-                    // si un produit et une meme option sont déjà present 
-                    if (mesProduitsEnregistrer[i].id === selectionUtilisateur.id && mesProduitsEnregistrer[i].choice === selectionUtilisateur.choice) {
-                    
-                        let totalQuantité = parseInt(mesProduitsEnregistrer[i].quantity, 10) + parseInt(selectionUtilisateur.quantity ,10);
-                        
-                        let updateStorage = mesProduitsEnregistrer;
-            
-                        updateStorage[i].quantity = totalQuantité;
-            
-                        mesProduitsEnregistrer = updateStorage;
-                        
-                        confirmationFonction();
-                        localStorage.setItem('mon panier', JSON.stringify(mesProduitsEnregistrer));
-                    
-                    // tant que l'on a pas fini de parcourir le tableau
-                    } else if ( (i + 1) < mesProduitsEnregistrer.length) {
-
-                        if (mesProduitsEnregistrer.length < 10 && mesProduitsEnregistrer.length > 0) {
-
-                            continue;
-
-                        // maximum article different 
-                        } else if ( mesProduitsEnregistrer.length == 10) {
-
-                            confirm.style.background ="red";
-                    
-                            confirm.textContent = `VOTRE PANIER EST PLEIN`;
-                            confirm.insertAdjacentHTML('beforeend', `<i class="far fa-times-circle"></i>`);
-                
-                            return;
-
-                        };
-                    
-                    // si le tableau et parcourus en integralité, et que aucun produit nest identique sur lid ou le choix on add
-                    } else if ((i + 1) == mesProduitsEnregistrer.length && mesProduitsEnregistrer[i].id != selectionUtilisateur.id || mesProduitsEnregistrer[i].choice != selectionUtilisateur.choice) {
-    
-                        confirmationFonction();
-                        localSto(selectionUtilisateur, mesProduitsEnregistrer);
-
-                    };
-
-                    return;
-
-                };
-
-            };
-  
-        };
-
-        verificationDesProduits();
+        verificationDesProduits(produit);
 
     });
 
 };
+
+/* ########################################################### */
+/* ----------------------------------------------------------- */
+
+
